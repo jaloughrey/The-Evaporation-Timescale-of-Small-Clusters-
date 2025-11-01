ac5d83e20d4db6bf709db5da7a7cf74bc000c794
jaloughrey <40250881+jaloughrey@users.noreply.github.com>
2025-11-01 19:16:02 +0000

    Update NBodyIntegrator.py

--- NBodyIntegrator.py
+++ NBodyIntegrator.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import glob 
 from numba import jit 
+import functions as func
 
 # MAIN
 
 #read parameter file for constants 
-N, G, radius, softening, eta, alpha_virial, mass_segregated, t_end, n_snap, loc, seed, stem, IC_stem = read_param('param_new.csv')
+N, G, radius, softening, eta, alpha_virial, mass_segregated, t_end, n_snap, loc, seed, stem, IC_stem = func.read_param('param.csv')
 
 #read the initial conidtions 
 IC_filename = f"IC_files/{IC_stem}_{seed}.csv"
 print(IC_filename)
-ID, M, x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, KE_ind, PE_ind, E_ind, bound, E_total, KE, PE, alpha_virial, t, dt, R90_initial = read_snapshot(IC_filename)
+ID, M, x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, KE_ind, PE_ind, E_ind, bound, E_total, KE, PE, alpha_virial, t, dt, R90_initial = func.read_snapshot(IC_filename)
     
 #initialise time variables for loop
 dt_snap = t_end/(n_snap+1) #time between snapshot files to meet desired number (n_snap)
@@ -24,7 +25,7 @@ while t < t_end:  #main loop from 0s to chosen end point
     
     if t >= t_snap: #create a snapshot at chosen interval
         snapshot_filename = f"snapshots/{loc}/SEED{seed}/{stem}_{snapshot_count:04d}.csv"
-        write_snapshot(snapshot_filename, x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, M, G, softening, t, dt, R90_initial)
+        func.write_snapshot(snapshot_filename, N, x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, M, G, softening, t, dt, R90_initial)
 
         #update variables
         t_snap += dt_snap 
@@ -36,7 +37,7 @@ while t < t_end:  #main loop from 0s to chosen end point
     #x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz = velocity_verlet(x, y, z, vx, vy, vz, ax, ay, az, M, G, dt)
     
     #hermite 4th order
-    x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, dt = hermite_4th_order(x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, M, G, dt, eta)
+    x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, dt = func.hermite_4th_order(N, x, y, z, vx, vy, vz, ax, ay, az, adotx, adoty, adotz, softening, M, G, dt, eta)
     
     t += dt #update current time
 
